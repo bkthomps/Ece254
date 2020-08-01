9e8aa4e4c66ea343d93cbc298de0fe28d87ce06d
bkthomps <bkthomps@uwaterloo.ca>
2020-08-01 03:26:01 -0400

    Update lab 3

--- lab3/starter/src/processes/produce.c
+++ lab3/starter/src/processes/produce.c
@@ -1,7 +1,3 @@
-// Use this to see if a number has an integer square root
-#define EPS 1.E-7
-
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,38 +9,155 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <math.h>
+#include <errno.h>
 
-double g_time[2];
+#define EPS			1.E-7
+#define TEN_TO_THE_SIX_DECIMAL	1000000.0
+#define QUEUE_NAME		"/processes_queue_ece254_fall_2018_group32_3"
+#define MAX_MSG_SIZE		256
 
-
-int main(int argc, char *argv[])
+void busy_loop(int iters)
 {
-	int num;
-	int maxmsg;
-	int num_p;
-	int num_c;
-	int i;
-	struct timeval tv;
+	volatile int sink;
+  	do {
+    		sink = 0;
+  	} while (--iters > 0);
+  	(void) sink;
+}
 
+static void producer(mqd_t queue,
+		     const int id,
+		     const int num_items,
+		     const int num_producers)
+{
+	int i;
+	// Put items into our message queue
+	for (i = id; i < num_items; i += num_producers) {
+		busy_loop(40000);
+		if (mq_send(queue, (char *) &i, sizeof(int), 0) < 0) {
+			printf("Message send failed\n");
+			exit(1);
+		}
+	}
+}
+
+static void consumer(mqd_t queue,
+		     const int cid,
+		     const int buffer_size,
+		     const int num_items,
+		     const int num_consumers)
+{
+	int i;
+	// Remove number of items from the queue corresponding to the cid
+	for (i = cid; i < num_items; i += num_consumers) {
+		int num;
+		if (mq_receive(queue, (char *) &num, sizeof(int), 0) < 0) {
+			printf("Message send failed\n");
+			exit(1);
+		}
+
+		busy_loop(50000);
+
+		// Check if the square root is an integer
+		const double val = sqrt(num);
+		if (fabs(val - round(val)) < EPS) {
+			printf("%d %d %d\n", cid, num, (int) round(val));
+		}
+
+	}
+}
+
+int main(int argc, char **argv)
+{
 	if (argc != 5) {
 		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
 		exit(1);
 	}
+	const int num_items	= atoi(argv[1]);
+	const int buffer_size	= atoi(argv[2]);
+	const int num_producers	= atoi(argv[3]);
+	const int num_consumers	= atoi(argv[4]);
 
-	num = atoi(argv[1]);	/* number of items to produce */
-	maxmsg = atoi(argv[2]); /* buffer size                */
-	num_p = atoi(argv[3]);  /* number of producers        */
-	num_c = atoi(argv[4]);  /* number of consumers        */
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	const double start_time_in_seconds
+		= (tv.tv_sec) + tv.tv_usec / TEN_TO_THE_SIX_DECIMAL;
 
+	int p_cnt = 0;
+	int c_cnt = 0;
+	// keep track of the children pids
+	int child_pid[num_producers + num_consumers];
+	int pid;
+	int i;
+
+	// Create Queue
+	struct mq_attr attr;
+	attr.mq_flags	= 0;
+	attr.mq_maxmsg	= buffer_size;
+	attr.mq_msgsize	= sizeof(int);
+	attr.mq_curmsgs	= 0;
+	mqd_t queue = mq_open(QUEUE_NAME, O_CREAT | O_RDWR, 0644, &attr);
+	if (queue < 0) {
+		printf("Queue open failed\n");
+		exit(1);
+	}
+
+	// parent process will fork children
+fork_process:
+	pid = fork();
+	if (pid < 0) {
+		printf("Fork failed\n");
+		exit(1);
+	} else if (pid) {	/* parent */
+		if (c_cnt < num_consumers) {
+			// while we're forking consumers
+			// save the pid of our children
+			child_pid[c_cnt] = pid;
+			c_cnt++;
+			goto fork_process;
+		}
+		if (p_cnt < num_producers - 1) {
+			// while we're forking producers
+			// save the pid of our children
+			child_pid[p_cnt + c_cnt] = pid;
+			p_cnt++;
+			goto fork_process;
+		}
+	} else {	/* child */
+		if (c_cnt < num_consumers) {
+			// produce consumers up to a certain amount
+			consumer(queue, c_cnt, buffer_size, num_items, num_consumers);
+		} else  {
+			// produce producers up to a certain amount
+			producer(queue, p_cnt, num_items, num_producers);
+		}
+		// Close queue
+		if (mq_close(queue) < 0) {
+			printf("Queue close failed\n");
+			exit(1);
+		}
+		exit(0);
+	}
+
+	// wait for all the children processes to complete
+	for (i = 0; i < num_producers + num_consumers; i++) {
+		waitpid(child_pid[i], NULL, 0);
+	}
+
+	// Close queue
+	if (mq_close(queue) < 0) {
+		printf("Queue close failed\n");
+		exit(1);
+	}
+	if (mq_unlink(QUEUE_NAME) < 0) {
+		printf("Unlink failed\n");
+		exit(1);
+	}
 
 	gettimeofday(&tv, NULL);
-	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
-
-
-    gettimeofday(&tv, NULL);
-    g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
-
-    printf("System execution time: %.6lf seconds\n", \
-            g_time[1] - g_time[0]);
+	const double end_time_in_seconds
+		= (tv.tv_sec) + tv.tv_usec / TEN_TO_THE_SIX_DECIMAL;
+	printf("System execution time: %.6lf seconds\n",
+	       end_time_in_seconds - start_time_in_seconds);
 	exit(0);
 }
--- lab3/starter/src/threads/produce.c
+++ lab3/starter/src/threads/produce.c
@@ -1,7 +1,3 @@
-// Use this to see if a number has an integer square root
-#define EPS 1.E-7
-
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,40 +7,155 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include <sys/wait.h>
 #include <math.h>
 
-double g_time[2];
+#define EPS			1.E-7
+#define TEN_TO_THE_SIX_DECIMAL	1000000.0
 
 
-int main(int argc, char *argv[])
+// Global Variables
+static int num_items;
+static int buffer_size;
+static int num_producers;
+static int num_consumers;
+
+static sem_t producer_sem;
+static sem_t consumer_sem;
+static pthread_mutex_t mutex;
+static int *buffer;
+static int head_index = 1;
+static int tail_index = 0;
+
+void busy_loop(int iters)
 {
-	int num;
-	int maxmsg;
-	int num_p;
-	int num_c;
-	int i;
-	struct timeval tv;
+	volatile int sink;
+  	do {
+    		sink = 0;
+  	} while (--iters > 0);
+  	(void) sink;
+}
 
+
+static void *producer(void *const param)
+{
+	// Get the process id
+	const int id = *(int *) param;
+	int i;
+
+	// Generate values to put in our buffer
+	for (i = id; i < num_items; i += num_producers) {
+		busy_loop(40000);
+		// decrement to indicate less space
+		sem_wait(&producer_sem);
+		// lock the mutex to ensure mutual exclusion
+		pthread_mutex_lock(&mutex);
+		// adding the item to the end of the queue
+		tail_index = (tail_index + 1) % buffer_size;
+		// update our tail index to indicate it's now occupied
+		buffer[tail_index] = i;
+		pthread_mutex_unlock(&mutex);
+		// increment to indicate item is put in buffer
+		sem_post(&consumer_sem);
+	}
+
+	// After generating all possible values finish
+	pthread_exit(param);
+}
+
+static void *consumer(void *const param)
+{
+	// Get the consumer id
+	const int cid = *(int *) param;
+	int i;
+	
+	// Remove a certain number of items from buffer corresp to cid
+	for (i = cid; i < num_items; i += num_consumers) {
+		// decrement to indicate less items in buffer
+		sem_wait(&consumer_sem);
+		// lock the mutex to ensure mutual exclusion
+		pthread_mutex_lock(&mutex);
+		// grab the head of the queue
+		const int num = buffer[head_index];
+		// remove it from the queue by updating our head
+		head_index = (head_index + 1) % buffer_size;
+		pthread_mutex_unlock(&mutex);
+		// check if the removed number is a square
+		busy_loop(50000);
+		
+		const double val = sqrt(num);
+		if (fabs(val - round(val)) < EPS) {
+			printf("%d %d %d\n", cid, num, (int) round(val));
+		}
+		// increment to indicate we have a space in the buffer
+		sem_post(&producer_sem);
+	}
+	// After removing all our allowed values finish
+	pthread_exit(param);
+
+}
+
+int main(int argc, char **argv)
+{
 	if (argc != 5) {
 		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
 		exit(1);
 	}
+	num_items	= atoi(argv[1]);
+	buffer_size	= atoi(argv[2]);
+	num_producers	= atoi(argv[3]);
+	num_consumers	= atoi(argv[4]);
 
-	num = atoi(argv[1]);	/* number of items to produce */
-	maxmsg = atoi(argv[2]); /* buffer size                */
-	num_p = atoi(argv[3]);  /* number of producers        */
-	num_c = atoi(argv[4]);  /* number of consumers        */
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	const double start_time_in_seconds
+		= (tv.tv_sec) + tv.tv_usec / TEN_TO_THE_SIX_DECIMAL;
+	
+	// keep track of the threads we are creating
+	pthread_t producers[num_producers];
+	pthread_t consumers[num_consumers];
+	int i;
+	buffer = malloc(buffer_size * sizeof(int));
 
+	// initialize our semaphores
+	pthread_mutex_init(&mutex, NULL);
+	// producer semaphore indicates number of spaces in buffer
+	sem_init(&producer_sem, 0, buffer_size);
+	// consumer semaphore indicates number of items in buffer
+	sem_init(&consumer_sem, 0, 0);
+
+	// creating the threads
+	for (i = 0; i < num_producers; i++) {
+		int *id = malloc(sizeof(int));
+		*id = i;
+		pthread_create(&producers[i], NULL, producer, id);
+	}
+	for (i = 0; i < num_consumers; i++) {
+		int *cid = malloc(sizeof(int));
+		*cid = i;
+		pthread_create(&consumers[i], NULL, consumer, cid);
+	}
+
+	// joining the threads
+	for (i = 0; i < num_producers; i++) {
+		void *ret;
+        	pthread_join(producers[i], &ret);
+        	free((int *) ret);
+	}
+	for (i = 0; i < num_consumers; i++) {
+		void *ret;
+		pthread_join(consumers[i], &ret);
+		free((int *) ret);
+	}
+
+	free(buffer);
 
 	gettimeofday(&tv, NULL);
-	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
-
-
-    gettimeofday(&tv, NULL);
-    g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
-
-    printf("System execution time: %.6lf seconds\n", \
-            g_time[1] - g_time[0]);
+	const double end_time_in_seconds
+		= (tv.tv_sec) + tv.tv_usec / TEN_TO_THE_SIX_DECIMAL;
+	printf("System execution time: %.6lf seconds\n", \
+	       end_time_in_seconds - start_time_in_seconds);
 	exit(0);
 }
